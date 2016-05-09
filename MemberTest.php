2243d840d7330478fd466c5ebba936ae5e101904
Jane Sandberg <sandbej@linnbenton.edu>
2016-05-09 06:49:10 -0700

    adding test to make sure bad member add returns error

--- MemberTest.php
+++ MemberTest.php
@@ -12,4 +12,12 @@ class MemberTest extends PHPUnit_Framework_TestCase {
 		$this->assertInternalType('array', json_decode($this->getJSONMembersBySearchQuery(), 1));
 	}
 
+	public function testAddMemberReturnsErrorWhenRequiredFieldsMissing() {
+                $url = OBIB_LOCAL_URL . "/circ/memberServer.php";
+                $opts = array('http' => array('method' => 'POST', 'content' => 'mode=addNewMember&lastName=BEAST&barcode_nmber=29191919'));
+                $context  = stream_context_create($opts);
+                $this->assertGreaterThan(0, strlen(file_get_contents($url, false, $context)));
+
+	}
+
 }
