1b086e4103674a3838159a17855b1205ae8ec063
Jane Sandberg <sandbej@linnbenton.edu>
2016-05-29 11:09:42 -0700

    adding calendar test

---
+++ CalendarTest.php
@@ -0,0 +1,14 @@
+<?php
+class CalendarTest extends PHPUnit_Framework_TestCase {
+
+	private function getRandomCalendar() {
+		return 2;
+	}
+
+	public function testCanGetHTMLCalendarDisplay() {
+		$url = OBIB_LOCAL_URL . "/admin/calendarSrvr.php";
+		$opts = array('http' => array('method' => 'POST', 'content' => 'mode=getCalendar&calendar='.getRandomCalendar()));
+		$context  = stream_context_create($opts);
+		$this->assertGreaterThan(0, strlen(file_get_contents($url, false, $context)));
+	}
+}
