92caa51c50521db5f6e8da053048fc9d1a4c724c
Jane Sandberg <sandbej@linnbenton.edu>
2016-05-29 17:09:02 -0700

    fixing calendar test, closes #1

--- CalendarTest.php
+++ CalendarTest.php
@@ -7,7 +7,7 @@ class CalendarTest extends PHPUnit_Framework_TestCase {
 
 	public function testCanGetHTMLCalendarDisplay() {
 		$url = OBIB_LOCAL_URL . "/admin/calendarSrvr.php";
-		$opts = array('http' => array('method' => 'POST', 'content' => 'mode=getCalendar&calendar='.getRandomCalendar()));
+		$opts = array('http' => array('method' => 'POST', 'content' => 'mode=getCalendar&calendar='.$this->getRandomCalendar()));
 		$context  = stream_context_create($opts);
 		$this->assertGreaterThan(0, strlen(file_get_contents($url, false, $context)));
 	}
